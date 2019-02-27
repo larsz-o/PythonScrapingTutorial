702b4c71426d46a8339f0aaef931af8190ba6de3
Lars <larsmackenzie@gmail.com>
2019-02-27 12:30:38 -0600

    first commit

---
+++ scrapy.py
@@ -0,0 +1,28 @@
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+
+from bs4 import BeautifulSoup
+
+# specify the url
+quote_page = 'http://www.bloomberg.com/quote/SPX:IND'
+
+# query the website and return the html to the variable page
+page =  urlopen(quote_page)
+
+# parse the html using beautiful soup and store in variable soup
+soup = BeautifulSoup(page, 'html.parser')
+print(soup)
+# # Take out the <div> of name and get its value
+name_box = soup.find('h1')
+print(name_box)
+name = name_box.text.strip() # strip() is used to remove starting and trailing
+print(name)
+
+# get the h2 value
+subtitle = soup.find('p', attrs={'class': 'continue'}) #, attrs={'class':'priceText__1853e8a5'}
+content = subtitle.text
+print(content)
