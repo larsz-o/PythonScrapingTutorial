b40c9c8127bc1a7081865cd6c0e303ae2a73cbb5
Lars <larsmackenzie@gmail.com>
2019-02-27 12:45:19 -0600

    scraping multiple pages with same attribute names functionality added

--- scrapy.py
+++ scrapy.py
@@ -9,16 +9,23 @@ from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
 
-
 # specify the url
-quote_page = 'http://www.bloomberg.com/quote/SPX:IND'
+quote_page = ['http://www.bloomberg.com/quote/SPX:IND', 'http://www.bloomberg.com/quote/CCMP:IND']
 
+# for scraping just one page:
 # query the website and return the html to the variable page
-page =  urlopen(quote_page)
+# page =  urlopen(quote_page)
 
-# parse the html using beautiful soup and store in variable soup
+# for loop
+data = []
+for pg in quote_page:
+ # query the website and return the html to the variable page
+ page = urlopen(pg)
+
+# parse the html using beautiful soup and store in variable soup 
 soup = BeautifulSoup(page, 'html.parser')
 print(soup)
+
 # # Take out the <div> of name and get its value
 name_box = soup.find('h1')
 print(name_box)
@@ -30,7 +37,18 @@ subtitle = soup.find('p', attrs={'class': 'continue'}) #, attrs={'class':'priceT
 content = subtitle.text
 print(content)
 
+# save the data in tuple
+data.append((name, content))
+
+# for adding data from just one site to a csv
+# open a csv file with append, so old data will not be erased
+# with open('index.csv', 'a') as csv_file:
+#     writer = csv.writer(csv_file)
+#     writer.writerow([name, content, datetime.now()])
+
 # open a csv file with append, so old data will not be erased
 with open('index.csv', 'a') as csv_file:
-    writer = csv.writer(csv_file)
+ writer = csv.writer(csv_file)
+ # The for loop
+ for name, content in data:
     writer.writerow([name, content, datetime.now()])
